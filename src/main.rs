607521294dbd081a64c555539dc6be147083fed2
KiefBC <kiefhay642@outlook.com>
2025-09-09 10:13:28 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,79 @@
+use std::collections::HashMap;
 use std::net::TcpListener;
 use std::{
     io::{BufRead, BufReader, Write},
     net::TcpStream,
 };
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum HttpVersion {
+    Http1_1,
+}
+
+#[derive(Debug, Clone)]
+pub struct HttpRequest {
+    pub method: HttpMethod,
+    pub target: String,
+    pub version: HttpVersion,
+    pub headers: HashMap<String, String>,
+}
+
+impl HttpRequest {
+    pub fn parse(request: Vec<String>) -> Result<Self, &'static str> {
+        let request_line: Vec<&str> = request[0].split_whitespace().collect();
+        if request_line.len() != 3 {
+            return Err("Invalid request line");
+        }
+
+        let method = match request_line[0] {
+            "GET" => HttpMethod::GET,
+            "POST" => HttpMethod::POST,
+            "PUT" => HttpMethod::PUT,
+            "DELETE" => HttpMethod::DELETE,
+            _ => return Err("Unsupported HTTP method"),
+        };
+
+        let target = request_line[1].to_string();
+        if target.is_empty() {
+            return Err("Empty request target");
+        }
+        // We only accept absolute path without query or fragment
+        if !target[1..].is_empty() {
+            return Err("Invalid request target");
+        }
+
+        let version = match request_line[2] {
+            "HTTP/1.1" => HttpVersion::Http1_1,
+            _ => return Err("Unsupported HTTP version"),
+        };
+
+        let mut headers: HashMap<String, String> = HashMap::new();
+        for line in &request[1..] {
+            if line.is_empty() {
+                break;
+            }
+
+            if let Some((key, value)) = line.split_once(": ") {
+                headers.insert(key.to_string(), value.to_string());
+            }
+        }
+
+        Ok(HttpRequest {
+            method,
+            target,
+            version,
+            headers,
+        })
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -11,7 +82,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!(
-                    "accepted new connection from {}",
+                    "\naccepted new connection from {}",
                     stream.peer_addr().unwrap()
                 );
                 handle_client(stream);
@@ -24,8 +95,7 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut request_lines = Vec::new();
-    let mut read_error = false;
+    let mut request_lines: Vec<String> = Vec::new();
 
     {
         let reader = BufReader::new(&stream);
@@ -35,30 +105,28 @@ fn handle_client(mut stream: TcpStream) {
                     if line.is_empty() {
                         break;
                     }
+                    // println!("read line: {}", line);
                     request_lines.push(line);
-                    println!("read line: {}", request_lines.last().unwrap());
                 }
                 Err(e) => {
                     println!("error reading line: {}", e);
-                    read_error = true;
                     break;
                 }
             }
         }
     }
 
-    if read_error {
-        let resp = "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
-        let _ = stream.write_all(resp.as_bytes());
-        return;
-    }
-
-    if let Some(first) = request_lines.first() {
-        println!("Request: {}\n", first);
-    }
-
-    let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
-    if let Err(e) = stream.write_all(response.as_bytes()) {
-        println!("error writing response: {}", e);
+    if let Ok(request) = HttpRequest::parse(request_lines) {
+        println!("parsed request: {:?}", request);
+        let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+        if let Err(e) = stream.write_all(response.as_bytes()) {
+            println!("error writing response: {}", e);
+        }
+    } else {
+        println!("failed to parse request");
+        let response = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+        if let Err(e) = stream.write_all(response.as_bytes()) {
+            println!("error writing response: {}", e);
+        }
     }
 }
