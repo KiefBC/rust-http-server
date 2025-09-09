817930d1e04723008a64981b52514e8ff4b11cd2
KiefBC <kiefhay642@outlook.com>
2025-09-09 08:48:39 -0700

    feat: initial starting point of http server
    - can initiate server
    - can accept client
    - reads in request, spits out response (only 200 for now)\

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,64 @@
-use std::io::Write;
-#[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                if let Err(e) = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n") {
-                    eprintln!("error: {}", e);
-                }
+            Ok(stream) => {
+                println!(
+                    "accepted new connection from {}",
+                    stream.peer_addr().unwrap()
+                );
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut request_lines = Vec::new();
+    let mut read_error = false;
+
+    {
+        let reader = BufReader::new(&stream);
+        for line_result in reader.lines() {
+            match line_result {
+                Ok(line) => {
+                    if line.is_empty() {
+                        break;
+                    }
+                    request_lines.push(line);
+                    println!("read line: {}", request_lines.last().unwrap());
+                }
+                Err(e) => {
+                    println!("error reading line: {}", e);
+                    read_error = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if read_error {
+        let resp = "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+        let _ = stream.write_all(resp.as_bytes());
+        return;
+    }
+
+    if let Some(first) = request_lines.first() {
+        println!("Request: {}\n", first);
+    }
+
+    let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\nConnection: close\r\n\r\n";
+    if let Err(e) = stream.write_all(response.as_bytes()) {
+        println!("error writing response: {}", e);
     }
 }
